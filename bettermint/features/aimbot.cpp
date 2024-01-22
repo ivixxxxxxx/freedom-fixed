5d8c0829ec3937158f22fba03915cb88a47c4659
ivixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx <151454323+ivixxxxxxx@users.noreply.github.com>
2024-01-22 10:33:22 -0500

    Update aimbot.cpp

--- bettermint/features/aimbot.cpp
+++ bettermint/features/aimbot.cpp
@@ -7,44 +7,11 @@ static float rand_range_f(float f_min, float f_max) {
     return f_min + scale * (f_max - f_min);
 }
 
-static inline float smoothStep(float edge0, float edge1, float x) {
-    float t = fmaxf(0.0, fminf(1.0, (x - edge0) / (edge1 - edge0)));
-    return t * t * (3.0 - 2.0 * t);
-}
-
-static inline float easeInOutQuad(float t) {
-    return t < 0.5 ? 2.0 * t * t : 1.0 - pow(-2.0 * t + 2.0, 2.0) / 2.0;
-}
-
-static inline float lerpWithEase(float a, float b, float t) {
-    t = smoothStep(0.0f, 1.0f, t);
-    return a + t * (b - a);
-}
-
 static constexpr float DEAD_ZONE_THRESHOLD = 1.0f;
 
 template <typename T>
 static inline T distance(const Vector2<T> &v1, const Vector2<T> &v2) {
     return std::sqrt(std::pow(v1.x - v2.x, 2) + std::pow(v1.y - v2.y, 2));
-}
-
-static inline void move_mouse_smoothly(const Vector2<float> &target, const Vector2<float> &cursor_pos, float t) {
-    Vector2<float> target_on_screen = playfield_to_screen(target);
-
-    // Calculate smooth movement
-    Vector2<float> delta = target_on_screen - cursor_pos;
-    Vector2<float> step = delta * t; // Adjust the multiplier for speed
-
-    // Calculate the distance between current cursor position and target
-    float distance_to_target = distance(cursor_pos, target_on_screen);
-
-    // If the distance is greater than a threshold, move smoothly; otherwise, teleport
-    if (distance_to_target > 1.0f) {
-        Vector2<float> predicted_position = cursor_pos + step;
-        move_mouse_to(predicted_position.x, predicted_position.y);
-    } else {
-        move_mouse_to(target_on_screen.x, target_on_screen.y);
-    }
 }
 
 static inline Vector2<float> stableMousePosition() {
@@ -56,25 +23,27 @@ static inline Vector2<float> stableMousePosition() {
     currentMousePos.x = *(float *)(osu_ruleset_ptr + OSU_RULESET_MOUSE_X_OFFSET);
     currentMousePos.y = *(float *)(osu_ruleset_ptr + OSU_RULESET_MOUSE_Y_OFFSET);
 
-    static Vector2<float> lastMousePos = currentMousePos;
-
-    if (distance(currentMousePos, lastMousePos) < DEAD_ZONE_THRESHOLD) {
-        return lastMousePos;
-    }
-
-    lastMousePos = currentMousePos;
     return currentMousePos;
+}
+
+static inline void move_mouse_smoothly(const Vector2<float> &target) {
+    Vector2<float> target_on_screen = playfield_to_screen(target);
+
+    // Calculate the distance between current cursor position and target
+    float distance_to_target = distance(stableMousePosition(), target_on_screen);
+
+    // If the distance is greater than a threshold, move smoothly; otherwise, teleport
+    if (distance_to_target > DEAD_ZONE_THRESHOLD) {
+        move_mouse_to(target_on_screen.x, target_on_screen.y);
+    }
 }
 
 void update_aimbot(Circle &circle, const int32_t audio_time) {
     if (!cfg_aimbot_lock)
         return;
 
-    float t = cfg_fraction_modifier * ImGui::GetIO().DeltaTime;
-    Vector2<float> cursor_pos = stableMousePosition();
-
     if (circle.type == HitObjectType::Circle) {
-        move_mouse_smoothly(circle.position, cursor_pos, t);
+        move_mouse_smoothly(circle.position);
     } else if (circle.type == HitObjectType::Slider) {
         uintptr_t osu_manager = *(uintptr_t *)(osu_manager_ptr);
         if (!osu_manager) return;
@@ -86,25 +55,25 @@ void update_aimbot(Circle &circle, const int32_t audio_time) {
         uintptr_t animation_ptr = *(uintptr_t *)(hit_object_ptr + OSU_HIT_OBJECT_ANIMATION_OFFSET);
         float slider_ball_x = *(float *)(animation_ptr + OSU_ANIMATION_SLIDER_BALL_X_OFFSET);
         float slider_ball_y = *(float *)(animation_ptr + OSU_ANIMATION_SLIDER_BALL_Y_OFFSET);
-        Vector2 slider_ball(slider_ball_x, slider_ball_y);
+        Vector2<float> slider_ball(slider_ball_x, slider_ball_y);
 
         float slider_variation = 5.0f;
         slider_ball.x += rand_range_f(-slider_variation, slider_variation);
         slider_ball.y += rand_range_f(-slider_variation, slider_variation);
 
-        move_mouse_smoothly(slider_ball, cursor_pos, t);
+        move_mouse_smoothly(slider_ball);
     } else if (circle.type == HitObjectType::Spinner && audio_time >= circle.start_time) {
         auto &center = circle.position;
         constexpr float radius = 60.0f;
         constexpr float PI = 3.14159f;
         static float angle = .0f;
-        Vector2 next_point_on_circle(center.x + radius * cosf(angle), center.y + radius * sinf(angle));
+        Vector2<float> next_point_on_circle(center.x + radius * cosf(angle), center.y + radius * sinf(angle));
 
         float spinner_variation = 10.0f;
         next_point_on_circle.x += rand_range_f(-spinner_variation, spinner_variation);
         next_point_on_circle.y += rand_range_f(-spinner_variation, spinner_variation);
 
-        move_mouse_smoothly(next_point_on_circle, cursor_pos, t);
+        move_mouse_smoothly(next_point_on_circle);
 
         float spin_variation = 0.1f;
         angle += cfg_spins_per_minute / (3 * PI) * ImGui::GetIO().DeltaTime + rand_range_f(-spin_variation, spin_variation);
