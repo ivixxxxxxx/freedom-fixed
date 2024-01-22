0171da3f3a1461ba5fe8e70cbc9e1bb82479a1b2
iVix <151454323+ivixxxxxxx@users.noreply.github.com>
2024-01-22 11:23:09 -0500

    Update aimbot.cpp

--- bettermint/features/aimbot.cpp
+++ bettermint/features/aimbot.cpp
@@ -34,6 +34,7 @@ static inline Vector2<float> randomizePosition(const Vector2<float> &position, f
 }
 
 static inline Vector2<float> moveTowards(const Vector2<float> &current, const Vector2<float> &target, float speed) {
+    // Move current position towards the target with a specified speed
     float delta_x = target.x - current.x;
     float delta_y = target.y - current.y;
     float distance_to_target = std::sqrt(delta_x * delta_x + delta_y * delta_y);
@@ -53,24 +54,9 @@ void update_aimbot(Circle &circle, const int32_t audio_time) {
     float t = cfg_fraction_modifier * ImGui::GetIO().DeltaTime;
     Vector2<float> cursor_pos = stableMousePosition();
 
-    if (circle.type == HitObjectType::Circle) {
-        Vector2<float> target = playfield_to_screen(randomizePosition(circle.position, 5.0f)); // Adjust variation as needed
-        cursor_pos = moveTowards(cursor_pos, target, 500.0f * t); // Adjust speed as needed
-    } else if (circle.type == HitObjectType::Slider) {
-        uintptr_t osu_manager = *(uintptr_t *)(osu_manager_ptr);
-        if (!osu_manager) return;
-        uintptr_t hit_manager_ptr = *(uintptr_t *)(osu_manager + OSU_MANAGER_HIT_MANAGER_OFFSET);
-        if (!hit_manager_ptr) return;
-        uintptr_t hit_objects_list_ptr = *(uintptr_t *)(hit_manager_ptr + OSU_HIT_MANAGER_HIT_OBJECTS_LIST_OFFSET);
-        uintptr_t hit_objects_list_items_ptr = *(uintptr_t *)(hit_objects_list_ptr + 0x4);
-        uintptr_t hit_object_ptr = *(uintptr_t *)(hit_objects_list_items_ptr + 0x8 + 0x4 * current_beatmap.hit_object_idx);
-        uintptr_t animation_ptr = *(uintptr_t *)(hit_object_ptr + OSU_HIT_OBJECT_ANIMATION_OFFSET);
-        float slider_ball_x = *(float *)(animation_ptr + OSU_ANIMATION_SLIDER_BALL_X_OFFSET);
-        float slider_ball_y = *(float *)(animation_ptr + OSU_ANIMATION_SLIDER_BALL_Y_OFFSET);
-        Vector2<float> slider_ball(slider_ball_x, slider_ball_y);
-
-        Vector2<float> target = playfield_to_screen(randomizePosition(slider_ball, 5.0f)); // Adjust variation as needed
-        cursor_pos = moveTowards(cursor_pos, target, 500.0f * t); // Adjust speed as needed
+    if (circle.type == HitObjectType::Circle || circle.type == HitObjectType::Slider) {
+        Vector2<float> target = playfield_to_screen(circle.position);
+        cursor_pos = moveTowards(cursor_pos, target, 500.0f * t);
     } else if (circle.type == HitObjectType::Spinner && audio_time >= circle.start_time) {
         auto &center = circle.position;
         constexpr float radius = 60.0f;
@@ -78,8 +64,8 @@ void update_aimbot(Circle &circle, const int32_t audio_time) {
         static float angle = .0f;
         Vector2<float> next_point_on_circle(center.x + radius * cosf(angle), center.y + radius * sinf(angle));
 
-        Vector2<float> target = playfield_to_screen(randomizePosition(next_point_on_circle, 10.0f)); // Adjust variation as needed
-        cursor_pos = moveTowards(cursor_pos, target, 500.0f * t); // Adjust speed as needed
+        Vector2<float> target = playfield_to_screen(next_point_on_circle);
+        cursor_pos = moveTowards(cursor_pos, target, 500.0f * t);
 
         float spin_variation = 0.1f;
         angle += cfg_spins_per_minute / (3 * PI) * ImGui::GetIO().DeltaTime + rand_range_f(-spin_variation, spin_variation);
