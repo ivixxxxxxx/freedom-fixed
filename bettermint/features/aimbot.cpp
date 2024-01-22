f3a1ca95d2077ffa589e456b9dafa67151cc2932
ivixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx <151454323+ivixxxxxxx@users.noreply.github.com>
2024-01-22 10:43:41 -0500

    Update aimbot.cpp

--- bettermint/features/aimbot.cpp
+++ bettermint/features/aimbot.cpp
@@ -45,23 +45,40 @@ void update_aimbot(Circle &circle, const int32_t audio_time) {
         Vector2<float> target = playfield_to_screen(circle.position);
         cursor_pos = smoothCursorMovement(cursor_pos, target, t);
     } else if (circle.type == HitObjectType::Slider) {
-        // ... (same as before)
-
+        uintptr_t osu_manager = *(uintptr_t *)(osu_manager_ptr);
+        if (!osu_manager) return;
+        uintptr_t hit_manager_ptr = *(uintptr_t *)(osu_manager + OSU_MANAGER_HIT_MANAGER_OFFSET);
+        if (!hit_manager_ptr) return;
+        uintptr_t hit_objects_list_ptr = *(uintptr_t *)(hit_manager_ptr + OSU_HIT_MANAGER_HIT_OBJECTS_LIST_OFFSET);
+        uintptr_t hit_objects_list_items_ptr = *(uintptr_t *)(hit_objects_list_ptr + 0x4);
+        uintptr_t hit_object_ptr = *(uintptr_t *)(hit_objects_list_items_ptr + 0x8 + 0x4 * current_beatmap.hit_object_idx);
+        uintptr_t animation_ptr = *(uintptr_t *)(hit_object_ptr + OSU_HIT_OBJECT_ANIMATION_OFFSET);
+        float slider_ball_x = *(float *)(animation_ptr + OSU_ANIMATION_SLIDER_BALL_X_OFFSET);
+        float slider_ball_y = *(float *)(animation_ptr + OSU_ANIMATION_SLIDER_BALL_Y_OFFSET);
         Vector2<float> slider_ball(slider_ball_x, slider_ball_y);
+
+        float slider_variation = 5.0f;
         slider_ball.x += rand_range_f(-slider_variation, slider_variation);
         slider_ball.y += rand_range_f(-slider_variation, slider_variation);
 
         Vector2<float> target = playfield_to_screen(slider_ball);
         cursor_pos = smoothCursorMovement(cursor_pos, target, t);
     } else if (circle.type == HitObjectType::Spinner && audio_time >= circle.start_time) {
-        // ... (same as before)
-
+        auto &center = circle.position;
+        constexpr float radius = 60.0f;
+        constexpr float PI = 3.14159f;
+        static float angle = .0f;
         Vector2<float> next_point_on_circle(center.x + radius * cosf(angle), center.y + radius * sinf(angle));
+
+        float spinner_variation = 10.0f;
         next_point_on_circle.x += rand_range_f(-spinner_variation, spinner_variation);
         next_point_on_circle.y += rand_range_f(-spinner_variation, spinner_variation);
 
         Vector2<float> target = playfield_to_screen(next_point_on_circle);
         cursor_pos = smoothCursorMovement(cursor_pos, target, t);
+
+        float spin_variation = 0.1f;
+        angle += cfg_spins_per_minute / (3 * PI) * ImGui::GetIO().DeltaTime + rand_range_f(-spin_variation, spin_variation);
     }
 
     move_mouse_to(cursor_pos.x, cursor_pos.y);
