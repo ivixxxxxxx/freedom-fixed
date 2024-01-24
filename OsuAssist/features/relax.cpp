de03fb9dbffcfdea3e303ec9b04ed2ac8b53b8f5
iVix <151454323+ivixxxxxxx@users.noreply.github.com>
2024-01-24 13:38:31 -0500

    Update relax.cpp

--- OsuAssist/features/relax.cpp
+++ OsuAssist/features/relax.cpp
@@ -8,8 +8,8 @@ float od_check_ms = .0f;
 
 float jumping_window_offset = .0f;
 
-int wait_hitobjects_min = 3;
-int wait_hitobjects_max = 45;
+int wait_hitobjects_min = 1;
+int wait_hitobjects_max = 80;
 
 bool debug_relax = false;
 
