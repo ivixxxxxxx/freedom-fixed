576bf829f8562e7678e6d280b714a8ea353850a0
iVix <151454323+ivixxxxxxx@users.noreply.github.com>
2024-01-24 02:57:05 -0500

    Update aimbot.cpp

--- bettermint/features/aimbot.cpp
+++ bettermint/features/aimbot.cpp
@@ -66,7 +66,7 @@ namespace aimbot {
 
         static Vector2<float> lastMousePos = currentMousePos;
 
-        if (distance(currentMousePos, lastMousePos) < DEAD_ZONE_THRESHOLD) {
+if (aimbot::distance<float>(currentMousePos, lastMousePos) < DEAD_ZONE_THRESHOLD) {
             return lastMousePos;
         }
 
