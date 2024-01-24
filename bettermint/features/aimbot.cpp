4bbd98382adc22b634ab8bb21d44c5750aada10a
iVix <151454323+ivixxxxxxx@users.noreply.github.com>
2024-01-24 00:27:25 -0500

    Update aimbot.cpp

--- bettermint/features/aimbot.cpp
+++ bettermint/features/aimbot.cpp
@@ -4,9 +4,9 @@
 #include <cmath>
 #include <cstdlib>
 
-namespace autobot {
+namespace aimbot {
 
-    constexpr static u32 MODULE_ID{1};
+    constexpr static u32 MODULE_ID{ 1 };
 
     u8 active{};
     u32 last_top_note{};
@@ -31,18 +31,18 @@ namespace autobot {
     }
 
     template <typename T>
-    static inline T distance(const Vector2<T> &v1, const Vector2<T> &v2) {
+    static inline T distance(const Vector2<T>& v1, const Vector2<T>& v2) {
         return std::sqrt(std::pow(v1.x - v2.x, 2) + std::pow(v1.y - v2.y, 2));
     }
 
     static inline Vector2<float> stableMousePosition() {
         Vector2<float> currentMousePos(.0f, .0f);
-        uintptr_t osu_manager = *(uintptr_t *)(osu_manager_ptr);
+        uintptr_t osu_manager = *(uintptr_t*)(osu_manager_ptr);
         if (!osu_manager) return currentMousePos;
-        uintptr_t osu_ruleset_ptr = *(uintptr_t *)(osu_manager + OSU_MANAGER_RULESET_PTR_OFFSET);
+        uintptr_t osu_ruleset_ptr = *(uintptr_t*)(osu_manager + OSU_MANAGER_RULESET_PTR_OFFSET);
         if (!osu_ruleset_ptr) return currentMousePos;
-        currentMousePos.x = *(float *)(osu_ruleset_ptr + OSU_RULESET_MOUSE_X_OFFSET);
-        currentMousePos.y = *(float *)(osu_ruleset_ptr + OSU_RULESET_MOUSE_Y_OFFSET);
+        currentMousePos.x = *(float*)(osu_ruleset_ptr + OSU_RULESET_MOUSE_X_OFFSET);
+        currentMousePos.y = *(float*)(osu_ruleset_ptr + OSU_RULESET_MOUSE_Y_OFFSET);
 
         static Vector2<float> lastMousePos = currentMousePos;
 
@@ -54,18 +54,19 @@ namespace autobot {
         return currentMousePos;
     }
 
-    static inline void move_mouse_to_target(const Vector2<float> &target, const Vector2<float> &cursor_pos, float t) {
+    static inline void move_mouse_to_target(const Vector2<float>& target, const Vector2<float>& cursor_pos, float t) {
         Vector2 target_on_screen = playfield_to_screen(target);
 
         float movement_variation = 1.5f; // Adjust as needed
         target_on_screen.x += rand_range_f(-movement_variation, movement_variation);
         target_on_screen.y += rand_range_f(-movement_variation, movement_variation);
 
-        Vector2 predicted_position(lerpWithEase(cursor_pos.x, target_on_screen.x, t), lerpWithEase(cursor_pos.y, target_on_screen.y, t));
+        Vector2 predicted_position(lerpWithEase(cursor_pos.x, target_on_screen.x, t),
+            lerpWithEase(cursor_pos.y, target_on_screen.y, t));
         move_mouse_to(predicted_position.x, predicted_position.y);
     }
 
-    void update_aimbot(Circle &circle, const int32_t audio_time) {
+    void update_aimbot(Circle& circle, const int32_t audio_time) {
         if (!cfg_aimbot_lock)
             return;
 
@@ -74,17 +75,18 @@ namespace autobot {
 
         if (circle.type == HitObjectType::Circle) {
             move_mouse_to_target(circle.position, cursor_pos, t);
-        } else if (circle.type == HitObjectType::Slider) {
-            uintptr_t osu_manager = *(uintptr_t *)(osu_manager_ptr);
+        }
+        else if (circle.type == HitObjectType::Slider) {
+            uintptr_t osu_manager = *(uintptr_t*)(osu_manager_ptr);
             if (!osu_manager) return;
-            uintptr_t hit_manager_ptr = *(uintptr_t *)(osu_manager + OSU_MANAGER_HIT_MANAGER_OFFSET);
+            uintptr_t hit_manager_ptr = *(uintptr_t*)(osu_manager + OSU_MANAGER_HIT_MANAGER_OFFSET);
             if (!hit_manager_ptr) return;
-            uintptr_t hit_objects_list_ptr = *(uintptr_t *)(hit_manager_ptr + OSU_HIT_MANAGER_HIT_OBJECTS_LIST_OFFSET);
-            uintptr_t hit_objects_list_items_ptr = *(uintptr_t *)(hit_objects_list_ptr + 0x4);
-            uintptr_t hit_object_ptr = *(uintptr_t *)(hit_objects_list_items_ptr + 0x8 + 0x4 * current_beatmap.hit_object_idx);
-            uintptr_t animation_ptr = *(uintptr_t *)(hit_object_ptr + OSU_HIT_OBJECT_ANIMATION_OFFSET);
-            float slider_ball_x = *(float *)(animation_ptr + OSU_ANIMATION_SLIDER_BALL_X_OFFSET);
-            float slider_ball_y = *(float *)(animation_ptr + OSU_ANIMATION_SLIDER_BALL_Y_OFFSET);
+            uintptr_t hit_objects_list_ptr = *(uintptr_t*)(hit_manager_ptr + OSU_HIT_MANAGER_HIT_OBJECTS_LIST_OFFSET);
+            uintptr_t hit_objects_list_items_ptr = *(uintptr_t*)(hit_objects_list_ptr + 0x4);
+            uintptr_t hit_object_ptr = *(uintptr_t*)(hit_objects_list_items_ptr + 0x8 + 0x4 * current_beatmap.hit_object_idx);
+            uintptr_t animation_ptr = *(uintptr_t*)(hit_object_ptr + OSU_HIT_OBJECT_ANIMATION_OFFSET);
+            float slider_ball_x = *(float*)(animation_ptr + OSU_ANIMATION_SLIDER_BALL_X_OFFSET);
+            float slider_ball_y = *(float*)(animation_ptr + OSU_ANIMATION_SLIDER_BALL_Y_OFFSET);
             Vector2 slider_ball(slider_ball_x, slider_ball_y);
 
             float slider_variation = 5.0f;
@@ -92,8 +94,9 @@ namespace autobot {
             slider_ball.y += rand_range_f(-slider_variation, slider_variation);
 
             move_mouse_to_target(slider_ball, cursor_pos, t);
-        } else if (circle.type == HitObjectType::Spinner && audio_time >= circle.start_time) {
-            auto &center = circle.position;
+        }
+        else if (circle.type == HitObjectType::Spinner && audio_time >= circle.start_time) {
+            auto& center = circle.position;
             constexpr float radius = 60.0f;
             constexpr float PI = 3.14159f;
             static float angle = .0f;
@@ -109,107 +112,5 @@ namespace autobot {
             angle += cfg_spins_per_minute / (3 * PI) * ImGui::GetIO().DeltaTime + rand_range_f(-spin_variation, spin_variation);
         }
     }
-
-    void auto_bot(osu_GameMode_Player* gamemode) {
-
-        virtual_mouse.active = 0;
-        virtual_keyboard.active = 1;
-
-        const int time = *osu_data.time;
-
-        const auto* hit_manager = gamemode->hitobject_manager;
-
-        const int press_delta = hit_manager->hit_window_300 - 1;
-
-        auto* top_note = hit_manager->get_top_note();
-
-        if (top_note == 0 || (press_delta + time - top_note->time[0]) < 0)
-            return;
-
-        vec2 target_pos{ top_note->pos };
-
-        if (top_note->type & Slider) {
-
-            auto* slider_ball = ((osu_Hitobject_SliderOsu*)top_note)->slider_ball;
-
-            if (slider_ball) {
-                target_pos.x = slider_ball->position.x;
-                target_pos.y = slider_ball->position.y;
-            }
-
-        } else if (top_note->type & Spinner) {
-
-            constexpr static float spin_r = 0.001f;
-            const auto t = float(time & 63) / 63.f;
-
-            target_pos.x += cos(-3.141592f + 2.f * 3.141592f * t) * spin_r;
-            target_pos.y += sin(-3.141592f + 2.f * 3.141592f * t) * spin_r;
-
-        }
-
-        if (last_top_note != u32(top_note)) {
-
-            last_top_note = u32(top_note);
-
-            if (virtual_keyboard.K1) {
-                virtual_keyboard.K1 = 0;
-                virtual_keyboard.K2 = 1;
-            } else {
-                virtual_keyboard.K1 = 1;
-                virtual_keyboard.K2 = 0;
-            }
-
-        }
-
-        virtual_mouse.active = 1;
-
-        virtual_mouse.pos = osu_window::field_to_display(target_pos);
-
-    }
-
-    void __fastcall update() {
-
-        if (active == 0 || *osu_data.play_mode != 0)
-            return;
-
-        const auto gamemode = (osu_GameMode_Player*)osu_data.running_gamemode[0];
-        
-        if (gamemode->async_load_complete == 0 || gamemode->game->is_unsafe() || gamemode->hitobject_manager == 0)
-            return;
-
-        auto_bot(gamemode);
-
-    }
-
-    void __fastcall menu_init() {
-
-        auto& menu = AQM::module_menu[MODULE_ID];
-
-        menu.sprite_list.reserve(64);
-
-        menu.name = "Autobot"sv;
-        menu.icon = FontAwesome::keyboard_o;
-
-        menu.colour = _col{ 16, 74, 168 , 255 };
-
-        {
-            menu_object mo{};
-
-            mo.name = "Enabled"sv;
-            mo.type = menu_object_type::clicker_bool;
-            mo.clicker_bool.value = &active;
-
-            menu.menu_elements.push_back(mo);
-        }
-
-    }
-
-    const auto initialized = [] {
-
-        on_audio_tick_ingame[MODULE_ID] = update;
-        on_menu_init[MODULE_ID] = menu_init;
-
-        return 1;
-    }();
 
 }
