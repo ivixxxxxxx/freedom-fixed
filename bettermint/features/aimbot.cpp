679c0f0fad4f36d63c24a6d50bd01b2b9fe1a80a
iVix <151454323+ivixxxxxxx@users.noreply.github.com>
2024-01-24 02:44:09 -0500

    Update aimbot.cpp

--- bettermint/features/aimbot.cpp
+++ bettermint/features/aimbot.cpp
@@ -51,7 +51,7 @@ T aimbot::distance(const Vector2<T>& v1, const Vector2<T>& v2) {
 
 // Add explicit instantiation for float
 template <>
-float aimbot::distance<float>(const Vector2<float>& v1, const Vector2<float>& v2) {
+float aimbot::distance(const Vector2<float>& v1, const Vector2<float>& v2) {
     return std::sqrt(std::pow(v1.x - v2.x, 2) + std::pow(v1.y - v2.y, 2));
 }
 
