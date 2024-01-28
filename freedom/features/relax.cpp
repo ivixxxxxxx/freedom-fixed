babeb2ff268d7119eaf0d18dc301f119aa9b091e
iVix <151454323+ivixxxxxxx@users.noreply.github.com>
2024-01-27 21:25:25 -0500

    Update relax.cpp

--- freedom/features/relax.cpp
+++ freedom/features/relax.cpp
@@ -15,17 +15,19 @@ bool debug_relax = false;
 
 static char current_click = cfg_relax_style == 'a' ? right_click[0] : left_click[0];
 
+static const auto rand_range_f = [](float f_min, float f_max) -> float
+{
+    float scale = rand() / (float)RAND_MAX;
+    return f_min + scale * (f_max - f_min);
+};
+
+static const auto rand_range_i = [](int i_min, int i_max) -> int
+{
+    return rand() % (i_max + 1 - i_min) + i_min;
+};
+
 void calc_od_timing()
 {
-    static const auto rand_range_f = [](float f_min, float f_max) -> float
-    {
-        float scale = rand() / (float)RAND_MAX;
-        return f_min + scale * (f_max - f_min);
-    };
-    static const auto rand_range_i = [](int i_min, int i_max) -> int
-    {
-        return rand() % (i_max + 1 - i_min) + i_min;
-    };
     if (cfg_relax_checks_od && (od_check_ms == .0f))
     {
         od_check_ms = rand_range_f(od_window_left_offset, od_window_right_offset);
@@ -35,7 +37,6 @@ void calc_od_timing()
             static int wait_hitojects_count = rand_range_i(wait_hitobjects_min, wait_hitobjects_max);
             if (current_beatmap.hit_object_idx - hit_objects_passed >= wait_hitojects_count)
             {
-                // NOTE(Ciremun): move od window to the left
                 if (rand_range_i(0, 1) >= 1)
                     jumping_window_offset = rand_range_f(.1337f, od_window - od_window_left_offset);
                 else
@@ -78,17 +79,21 @@ void update_relax(Circle &circle, const int32_t audio_time)
         if (debug_relax)
         {
             ImGui::GetBackgroundDrawList()->AddCircleFilled(
-            ImVec2(screen_pos.x, screen_pos.y),
-            current_beatmap.scaled_hit_object_radius,
-            ImColor( 0, 255, 255, 100 ) );
+                ImVec2(screen_pos.x, screen_pos.y),
+                current_beatmap.scaled_hit_object_radius,
+                ImColor(0, 255, 255, 100));
         }
 
-        if (valid_timing /* && valid_position */)
+        if (valid_timing && valid_position)
         {
             if (!circle.clicked)
             {
-                if (cfg_relax_style == 'a')
-                    current_click = current_click == left_click[0] ? right_click[0] : left_click[0];
+                if (rand_range_i(0, 1) >= 1)
+                    cfg_relax_style = 'a';
+                else
+                    cfg_relax_style = 'b';
+
+                current_click = cfg_relax_style == 'a' ? right_click[0] : left_click[0];
 
                 send_keyboard_input(current_click, 0);
                 FR_INFO_FMT("Relax hit %d!, %d %d", current_beatmap.hit_object_idx, circle.start_time, circle.end_time);
