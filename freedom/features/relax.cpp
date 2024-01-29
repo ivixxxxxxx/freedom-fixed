b16493d7d0e1456a70084f2f8bc0dbfc60b5c8c8
iVix <151454323+ivixxxxxxx@users.noreply.github.com>
2024-01-29 18:35:54 -0500

    Update relax.cpp

--- freedom/features/relax.cpp
+++ freedom/features/relax.cpp
@@ -70,55 +70,58 @@ void update_relax(Circle &circle, const int32_t audio_time)
     {
         calc_od_timing();
 
-        auto current_time = audio_time + od_check_ms + rand_range_f(-0.035f, 0.035f);
-        auto valid_timing = current_time >= circle.start_time;
-        auto mouse_pos = mouse_position();
-        Vector2 screen_pos = playfield_to_screen(circle.position);
-        auto scalar_dist = sqrt((mouse_pos.x - screen_pos.x) * (mouse_pos.x - screen_pos.x) + (mouse_pos.y - screen_pos.y) * (mouse_pos.y - screen_pos.y));
-        auto valid_position = scalar_dist <= current_beatmap.scaled_hit_object_radius;
-
-        if (debug_relax)
+        if (circle.valid)
         {
-            ImGui::GetBackgroundDrawList()->AddCircleFilled(
-                ImVec2(screen_pos.x, screen_pos.y),
-                current_beatmap.scaled_hit_object_radius,
-                ImColor(0, 255, 255, 100));
-        }
+            auto current_time = audio_time + od_check_ms + rand_range_f(-0.035f, 0.035f);
+            auto valid_timing = current_time >= circle.start_time;
+            auto mouse_pos = mouse_position();
+            Vector2 screen_pos = playfield_to_screen(circle.position);
+            auto scalar_dist = sqrt((mouse_pos.x - screen_pos.x) * (mouse_pos.x - screen_pos.x) + (mouse_pos.y - screen_pos.y) * (mouse_pos.y - screen_pos.y));
+            auto valid_position = scalar_dist <= current_beatmap.scaled_hit_object_radius;
 
-        if (valid_timing && valid_position)
-        {
-            if (!circle.clicked)
+            if (debug_relax)
             {
-                if (rand_range_i(1, 100) <= 13)
+                ImGui::GetBackgroundDrawList()->AddCircleFilled(
+                    ImVec2(screen_pos.x, screen_pos.y),
+                    current_beatmap.scaled_hit_object_radius,
+                    ImColor(0, 255, 255, 100));
+            }
+
+            if (valid_timing && valid_position)
+            {
+                if (!circle.clicked)
                 {
-                    keyup_delay = rand_range_f(0.0f, 0.013f);
-                }
-                else
-                {
-                    keyup_delay = circle.end_time ? circle.end_time - circle.start_time : 0.5;
-
-                    if (cfg_timewarp_enabled)
+                    if (rand_range_i(1, 100) <= 13)
                     {
-                        double timewarp_playback_rate_div_100 = cfg_timewarp_playback_rate / 100.0;
-                        keyup_delay /= timewarp_playback_rate_div_100;
+                        keyup_delay = rand_range_f(0.0f, 0.013f);
                     }
-                    else if (circle.type == HitObjectType::Slider || circle.type == HitObjectType::Spinner)
+                    else
                     {
-                        if (current_beatmap.mods & Mods::DoubleTime)
-                            keyup_delay /= 1.5;
-                        else if (current_beatmap.mods & Mods::HalfTime)
-                            keyup_delay /= 0.75;
+                        keyup_delay = circle.end_time ? circle.end_time - circle.start_time : 0.5;
+
+                        if (cfg_timewarp_enabled)
+                        {
+                            double timewarp_playback_rate_div_100 = cfg_timewarp_playback_rate / 100.0;
+                            keyup_delay /= timewarp_playback_rate_div_100;
+                        }
+                        else if (circle.type == HitObjectType::Slider || circle.type == HitObjectType::Spinner)
+                        {
+                            if (current_beatmap.mods & Mods::DoubleTime)
+                                keyup_delay /= 1.5;
+                            else if (current_beatmap.mods & Mods::HalfTime)
+                                keyup_delay /= 0.75;
+                        }
                     }
+
+                    current_click = (rand_range_i(0, 1) >= 1) ? right_click[0] : left_click[0];
+
+                    send_keyboard_input(current_click, 0);
+                    FR_INFO_FMT("Relax hit %d!, %d %d", current_beatmap.hit_object_idx, circle.start_time, circle.end_time);
+
+                    keydown_time = ImGui::GetTime();
+                    circle.clicked = true;
+                    od_check_ms = .0f;
                 }
-
-                current_click = (rand_range_i(0, 1) >= 1) ? right_click[0] : left_click[0];
-
-                send_keyboard_input(current_click, 0);
-                FR_INFO_FMT("Relax hit %d!, %d %d", current_beatmap.hit_object_idx, circle.start_time, circle.end_time);
-
-                keydown_time = ImGui::GetTime();
-                circle.clicked = true;
-                od_check_ms = .0f;
             }
         }
     }
